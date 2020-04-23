740519d6cbb0a9041286f2a3f848d15d5017ad53
Uday Maurya <infonitw@gmail.com>
2020-04-23 22:00:20 +0530

    initial commit

---
+++ src/main/java/com/dream/TutorialCreator/TutorialCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.dream.TutorialCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/TutorialCreator/TutorialCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.TutorialCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
