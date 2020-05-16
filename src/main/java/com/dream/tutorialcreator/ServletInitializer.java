7111e13a0cc38fe3c357f4e34a6862afdb77f218
lalitdaga <mr.lalitdaga@gmail.com>
2020-05-16 15:51:06 +0530

    change project structure
    change project structure

--- src/main/java/com/dream/tutorialcreator/ServletInitializer.java
+++ src/main/java/com/dream/tutorialcreator/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.dream.TutorialCreator;
+package com.dream.tutorialcreator;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
