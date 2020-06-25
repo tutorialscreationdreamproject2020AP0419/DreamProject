ab043b35a775152f6c61483992e5a2a9930d8a57
tutorialscreationdreamproject2020AP0419 <infonitw@gmail.com>
2020-06-25 23:54:21 +0530

    sql script for default user

--- deployment/sqlScript/00.00.01.sql
+++ deployment/sqlScript/00.00.01.sql
@@ -1,1 +1,13 @@
 ----------------------------------------Write your all DB related change here-----------------------------------------------
+create table mysql.users (id int, 
+						active TINYINT, 
+						password varchar(500), 
+                        role varchar(30), 
+                        user_name varchar(50),
+                        ISACCOUNTNONEXPIRED TINYINT,
+                        ISACCOUNTNONLOCKED TINYINT,
+                        ISCREDIANTIALSNONEXPIRED TINYINT
+                        );
+
+insert into mysql.users (id, active, password, role, user_name,ISACCOUNTNONEXPIRED,ISACCOUNTNONLOCKED,ISCREDIANTIALSNONEXPIRED) 
+						values (2, 1, "9044773351","ROLE_USER", "user",1,1,1); 
