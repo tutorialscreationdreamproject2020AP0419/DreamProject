f1fdb4b05bc8a7d5d748c65a590736f45dfd39a9
tutorialscreationdreamproject2020AP0419 <infonitw@gmail.com>
2020-08-16 13:59:04 +0530

    Changing column name

--- deployment/sqlScript/00.00.01.sql
+++ deployment/sqlScript/00.00.01.sql
@@ -3,11 +3,11 @@ create table mysql.users (id int,
 						active TINYINT, 
 						password varchar(500), 
                         role varchar(30), 
-                        user_name varchar(50),
+                        username varchar(50),
                         ISACCOUNTNONEXPIRED TINYINT,
                         ISACCOUNTNONLOCKED TINYINT,
                         ISCREDIANTIALSNONEXPIRED TINYINT
                         );
 
-insert into mysql.users (id, active, password, role, user_name,ISACCOUNTNONEXPIRED,ISACCOUNTNONLOCKED,ISCREDIANTIALSNONEXPIRED) 
+insert into mysql.users (id, active, password, role, username,ISACCOUNTNONEXPIRED,ISACCOUNTNONLOCKED,ISCREDIANTIALSNONEXPIRED) 
 						values (2, 1, "9044773351","ROLE_USER", "user",1,1,1); 
